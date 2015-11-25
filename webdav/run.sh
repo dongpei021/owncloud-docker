fb2e318215194265b4796949920b5e1ac0b0a674
dpdpdpdp <dp@dpdpdpdp.com>
2015-11-25 21:39:22 +0800

    chown /var/webdav

--- webdav/run.sh
+++ webdav/run.sh
@@ -3,5 +3,6 @@
 htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
 chown root:www-data /etc/apache2/webdav.password
 chmod 640 /etc/apache2/webdav.password
+chown www-data:root /var/webdav
 
 apache2 -D FOREGROUND
